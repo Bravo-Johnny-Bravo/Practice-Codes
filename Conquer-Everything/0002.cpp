76e01e0ee293d0f444dec0c6635e1ab499bd33f1
Zirak Mondegari <codewizduo@gmail.com>
2018-08-15 13:21:54 +0530

    Update 0002.cpp

--- Conquer-Everything/0002.cpp
+++ Conquer-Everything/0002.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* result = dummy;
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* result = dummyHead;
         int carry = 0;
         while (l1 != NULL || l2 != NULL) {
-            result->next = new ListNode(0);
+            int x = (l1 != NULL) ? l1->val : 0;
+            int y = (l2 != NULL) ? l2->val : 0;
+            int sum = x + y + carry;
+            result->next = new ListNode(sum % 10);
             result = result->next;
-            int cur = carry;
+            carry = sum / 10;
             if (l1 != NULL) {
-                cur += l1->val;
                 l1 = l1->next;
             }
             if (l2 != NULL) {
-                cur += l2->val;
                 l2 = l2->next;
             }
-            result->val = (cur % 10);
-            carry = cur / 10;
         }
         if (carry) {
             result->next = new ListNode(carry);
         }
-        return dummy->next;
+        return dummyHead->next;
     }
 };
